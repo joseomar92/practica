1021852f4dde24ad0f8ac54f40a3142acfc7f7f6
Jose Omar <joseomar9219@gmail.com>
2019-09-09 21:52:21 -0500

    sube

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    Transform radius;
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;
+
+    float maxSpeed = 10;
+    float maxSteer = 1;
+
+    Transform target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;
+        radius = GameObject.Find("Area").transform;
+
+        float R = (radius.transform.position - transform.position).magnitude;
+
+
+        desired = (target.position - transform.position).normalized * maxSpeed;// desired -= para que huya del target
+        steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);
+
+        velocity +=steer * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+    }
+}
